3011850a828da19ef061f4672e44a86289b6a7a5
leandro <leandrojv83@gmail.com>
2020-03-03 22:01:06 -0300

    CURSO DANKI CODE - Jogo pong parte 1 e 2

---
+++ games/java/PongGame/src/com/game/pong/Player.java
@@ -0,0 +1,39 @@
+package com.game.pong;
+
+import java.awt.*;
+
+public class Player {
+
+    public boolean right = false, left = false;
+    public int x, y, width, height;
+
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.width = 40;
+        this.height = 10;
+    }
+
+    public void tick() {
+        if (right) {
+            x++;
+        } else if (left) {
+            x--;
+        }
+
+        if (x + width > Game.WIDTH) {
+            x = Game.WIDTH - width;
+        }
+
+        if (x < 0) {
+            x = 0;
+        }
+
+    }
+
+    public void render(Graphics g) {
+        g.setColor(Color.BLUE);
+        g.fillRect(x, y, this.width, this.height);
+    }
+
+}
