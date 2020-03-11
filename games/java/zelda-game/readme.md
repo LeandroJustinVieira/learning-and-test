d3007e6308ecf1520f33bd1fe9de1e2b6df81ff8
leandro <leandrojv83@gmail.com>
2020-03-10 21:58:51 -0300

    CURSO DANKI CODE -> Brainstorm e criando projeto CURSO DANKI CODE -> Setup e Graficos CURSO DANKI CODE -> Finalizando sprites CURSO DANKI CODE -> Classe spritesheet player e entity

---
+++ games/java/zelda-game/readme.md
@@ -0,0 +1,26 @@
+# JOGO TESTE FORMATO DO ZELDA
+
+### O que deve conter:
+
+####  Artes apra o jogo / Sprites;
+
+##### Utilizando o GIMP para Pixel Art:
+
+* Deve ser criado uma nova imagem;
+* Deve ser adicionado grid (Visualizar -> Exibir grade);
+* Para configurar o tamanho da grade (Imagem -> Configurar Grade);
+  * Padrão Imagem: width: 160, height: 160 - Padrão grade: width: 16, height: 16
+* Deve ser criado uma nova camada transparente;
+  * Camada -> Nova camada (Shitf + Ctrl + N);
+  * Selecionar trasparente sem cor;
+
+#### Sistema para shooting;
+#### Sistema para criação de mapa;
+#### Sistema de leveis;
+#### Inimigos no jogo;
+#### Boss batle;
+#### Coletar items;
+#### Aumentar ataque ou defesa de acordo com item;
+#### Sistema de dano;
+#### Sistema para salvar o jogo e carregar o jogo;
+#### Audio e musica;
---
+++ games/java/zelda-game/src/com/zelda/main/Game.java
@@ -0,0 +1,133 @@
+package com.zelda.main;
+
+import com.zelda.entities.Entity;
+import com.zelda.entities.Player;
+import com.zelda.graficos.SpriteSheet;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game extends Canvas implements Runnable {
+
+    private static final long serialVersionUID = 1L;
+    public static JFrame frame;
+    private Thread thread;
+    private boolean isRunning = true;
+    private final int WIDTH = 240;
+    private final int HEIGHT = 160;
+    private final int SCALE = 3;
+
+    private BufferedImage image;
+
+    public List<Entity> entites;
+    public SpriteSheet spriteSheet;
+
+
+    public Game() {
+        this.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+        this.initFrame();
+        image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+        entites = new ArrayList<>();
+        spriteSheet = new SpriteSheet("/spritesheet.png");
+
+        entites.add(new Player(0,0,16,16, spriteSheet.getSprite(32, 0, 16, 16)));
+    }
+
+    public void initFrame() {
+        frame = new JFrame("Game #1");
+        frame.add(this);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+
+    public synchronized void start() {
+        this.thread = new Thread(this);
+        this.isRunning = true;
+        this.thread.start();
+    }
+
+    public synchronized void stop() {
+        isRunning = false;
+        try {
+            thread.join();
+        } catch (InterruptedException ex) {
+        }
+    }
+
+    public static void main(String args[]) {
+        Game game = new Game();
+        game.start();
+    }
+
+    public void tick() {
+        for (int i = 0; i < entites.size(); i++) {
+            Entity entity = entites.get(i);
+            entity.tick();
+        }
+    }
+
+    public void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        //Renderizando background fundo preto, sempre é renderizando para sobreescrever todas as animanções
+        Graphics g = image.getGraphics();
+        g.setColor(new Color(19, 19, 19));
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+        //Renderização do jogo
+//        Graphics2D graphics2D = (Graphics2D) g;
+        for (int i = 0; i < entites.size(); i++) {
+            Entity entity = entites.get(i);
+            entity.render(g);
+        }
+
+        g.dispose();
+        g = bs.getDrawGraphics();
+        g.drawImage(image, 0, 0, WIDTH * SCALE, HEIGHT * SCALE, null);
+        bs.show();
+    }
+
+
+    @Override
+    public void run() {
+
+        //Lógica de atualização (tick) e renderização (render) de acordo com o FPS (60FPS)
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        int frames = 0;
+        double timer = System.currentTimeMillis();
+        while (isRunning) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            if (delta >= 1) {
+                tick();
+                render();
+                frames++;
+                delta--;
+            }
+
+            if (System.currentTimeMillis() - timer >= 1000) {
+                System.out.println("FPS: " + frames);
+                frames = 0;
+                timer = System.currentTimeMillis();
+            }
+        }
+
+        stop();
+    }
+}
